67e657abefdf1d1b6b8ddbe1a554302b4d70928d
Quevin Bambasi <quevinbambasi@pm.me>
2023-03-16 14:19:20 -0400

    Initial Commit

---
+++ war-challenge/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  war-challenge
+//
+//  Created by Quevin Bambasi on 1/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                
+                Spacer()
+                Button(action: {
+                    // Generate a number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    // Update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    // Update the score
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }
+                    else if cpuRand > playerRand {
+                        cpuScore += 1
+                    }
+                }, label: {
+                    Image("dealbutton")
+                })
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .padding(.bottom)
+                        Text(String(playerScore))
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .padding(.bottom)
+                        Text(String(cpuScore))
+                    }
+                    Spacer()
+                }.foregroundColor(Color.white)
+                Spacer()
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Quevin Bambasi on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
